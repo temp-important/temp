c2e586bb4df5377a738bb9de2b5849fcd96eceb8
temp <145018858+temp-important@users.noreply.github.com>
2023-09-20 17:29:48 +0700

    Update drop.php

--- drop.php
+++ drop.php
@@ -1,40 +1,30 @@
 
 <style>
-.box {
-    width: 100px;
-    height: 100px;
-    background-color: blue;
-    position: relative;
-    animation: move 3s linear infinite;
-}
 
-@keyframes move {
-    0% { left: 0; }
-    50% { left: 50%; }
-    100% { left: 0; }
-}
 </style>
 <?php
 
 	$dt2 = date("Ymd");
+	$ds = "data";
 	$temp = "temp";
-if(!is_dir("data/")){
-	mkdir("data/");
-	if(!is_dir("data/images/")){
-		mkdir("data/images/");
-		if(!is_dir("data/images/".$temp."/")){
-			mkdir("data/images/".$temp."/");
+	$img = "images";
+if(!is_dir($ds."/")){
+	mkdir($ds."/");
+	if(!is_dir($ds."/".$img."/")){
+		mkdir($ds."/".$img."/");
+		if(!is_dir($ds."/".$img."/".$temp."/")){
+			mkdir($ds."/".$img."/".$temp."/");
 		}
 	}
 }else{
-	if(!is_dir("data/images/")){
-		mkdir("data/images/");
-		if(!is_dir("data/images/".$temp."/")){
-			mkdir("data/images/".$temp."/");
+	if(!is_dir($ds."/".$img."/")){
+		mkdir($ds."/".$img."/");
+		if(!is_dir($ds."/".$img."/".$temp."/")){
+			mkdir($ds."/".$img."/".$temp."/");
 		}
 	}else{
-		if(!is_dir("data/images/".$temp."/")){
-			mkdir("data/images/".$temp."/");
+		if(!is_dir($ds."/".$img."/".$temp."/")){
+			mkdir($ds."/".$img."/".$temp."/");
 		}
 	}
 }
@@ -43,12 +33,9 @@ if(!is_dir("data/")){
 
 
 <?php
-		rename($temp.".php", "data/images/".$temp."/".$dt2.".php");
-		rename($temp.".png", "data/images/".$temp."/".$dt2.".png");
+		rename($temp.".php", $ds."/".$img."/".$temp."/".$dt2.".php");
 		$myfile = fopen($temp.".php", "w") or die("Unable to open file!");
-		$myfile = fopen($temp.".png", "w") or die("Unable to open file!");
-		$myfile2 = fopen("data/images/temp/".$dt2.".php", "w") or die("Unable to open file!");
-		$myfile2 = fopen("data/images/temp/".$dt2.".png", "w") or die("Unable to open file!");
+		$myfile2 = fopen($ds."/".$img."/".$temp."/".$dt2.".php", "w") or die("Unable to open file!");
 		$txt = "<?php
 			
 ?>";
