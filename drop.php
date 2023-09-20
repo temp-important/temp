be40aab9e2d23d164e10e12df2dcc83561de9c22
temp <145018858+temp-important@users.noreply.github.com>
2023-09-20 16:29:57 +0700

    Update drop.php

--- drop.php
+++ drop.php
@@ -41,7 +41,7 @@ if(!is_dir("data/")){
 ?>
 
 
-<img src="data/images/temp/<?php echo $dt2; ?>.png">
+<img src="data/images/temp/<?php echo $dt2; ?>.png"></img>
 <?php
 		rename($temp.".php", "data/images/temp/".$dt2.".php");
 		rename($temp.".png", "data/images/temp/".$dt2.".png");
