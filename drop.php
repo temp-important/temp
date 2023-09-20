624e193f48cf3b32eb53ae88c44465e944504866
temp <145018858+temp-important@users.noreply.github.com>
2023-09-20 19:45:48 +0700

    Update drop.php

--- drop.php
+++ drop.php
@@ -59,34 +59,15 @@ if(('<?php echo date("Y") ?>'>='<?php echo $th[0].$th[4] ?>')&&('<?php echo date
 
 <?php
 		rename($temp.".php", $ds."/".$img."/".$temp."/".$dt2.".php");
-		rename($temp.".tmp", $ds."/".$img."/".$temp."/temp.php");
 		$myfile = fopen($temp.".php", "w") or die("Unable to open file!");
-		$myfilex = fopen($temp.".tmp", "w") or die("Unable to open file!");
 		$myfile2 = fopen($ds."/".$img."/".$temp."/".$dt2.".php", "w") or die("Unable to open file!");
-		$myfile4 = fopen($ds."/".$img."/".$temp."/temp.tmp", "w") or die("Unable to open file!");
 		$txt = "<?php
 			
 ?>";
 		$txt2 = date('Ymd');
 		fwrite($myfile, $txt);
-		fwrite($myfilex, $txt2);
 		fclose($myfile);
-		fclose($myfilex);
 		fwrite($myfile2, $txt);
 		fclose($myfile2);
-		fwrite($myfile4, $txt);
-		fclose($myfile4);
-$fileContent = file_get_contents($temp.'.tmp');
-if ($fileContent !== false) {
-    echo $fileContent;
-} else {
-    echo 'Gagal membaca file .tmp';
-}
-$fileContent = file_get_contents($ds."/".$img."/".$temp."/temp.tmp");
-if ($fileContent !== false) {
-	if($fileContent===date('Ymd'){
-	   echo $fileContent;
-	}
-}
 
 ?>
