ae7ec08aead107bb94c2538241d3386cf4ff2e92
temp <145018858+temp-important@users.noreply.github.com>
2023-09-20 15:36:49 +0700

    Update drop.php

--- drop.php
+++ drop.php
@@ -1,3 +1,20 @@
+
+<style>
+.box {
+    width: 100px;
+    height: 100px;
+    background-color: blue;
+    position: relative;
+    animation: move 3s linear infinite;
+}
+
+@keyframes move {
+    0% { left: 0; }
+    50% { left: 50%; }
+    100% { left: 0; }
+}
+</style>
+<div class="box"></div>
 <?php
 
 if(!is_dir("data/")){
